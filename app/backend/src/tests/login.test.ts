1013578de1738127f9a616196e6dafddaf5fd962
RicardoCaselati <ricardocaselati@gmail.com>
2022-12-08 15:30:45 -0300

    será que vai agora

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -10,7 +10,7 @@ import User from '../database/models/User'
 import {
   loginUser,
   loginMockResponse
-} from './login.mock'
+} from './util/mocks/allmocks'
 
 import { Response } from 'superagent';
 
